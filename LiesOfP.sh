97c2fc95850d338ca8d31d9b343052eceaf1f215
Jorge <jorge@localhost>
2026-05-02 05:19:41 +0200

    fix: update launch scripts with export vars and devinfo,fps,frametimes HUD

--- LiesOfP.sh
+++ LiesOfP.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Lies of P - Launch with NVIDIA RTX 3070 eGPU + FPS counter
-__NV_PRIME_RENDER_OFFLOAD=1 \
-__GLX_VENDOR_LIBRARY_NAME=nvidia \
-__VK_LAYER_NV_optimus=NVIDIA_only \
-DXVK_HUD=fps \
+# NOTE: Set these as Launch Options in Steam instead if this doesn't work:
+# __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only DXVK_HUD=devinfo,fps,frametimes %command%
+
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+export DXVK_HUD=devinfo,fps,frametimes
+
 steam steam://rungameid/1627720
--- LiesOfP_noegpu.sh
+++ LiesOfP_noegpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Lies of P - Launch with AMD Radeon (no eGPU) + FPS counter
-DXVK_HUD=fps \
+export DXVK_HUD=devinfo,fps,frametimes
+
 steam steam://rungameid/1627720
